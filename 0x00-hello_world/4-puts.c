450d326eb81a32240cbfe46878528782edc1a37d
abdelrahmane01 <abdelrahmanemam01@gmail.com>
2023-06-15 04:03:45 -0700

    bye

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Description: A C Program that prints with puts function
+ * Description: A C program that prints with put function
  * 
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
-int main (void)
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle \n");
 	return (0);
