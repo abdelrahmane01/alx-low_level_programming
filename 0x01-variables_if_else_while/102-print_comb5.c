6ab300b3f775ef71f451a6f08036686a362198df
abdelrahmane01 <abdelrahmanemam01@gmail.com>
2023-06-16 11:42:49 -0700

    bye

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,53 +10,32 @@
 
 int main(void)
 {
-	int digit1 = 0, digit2, digit3, digit4;
+	int digit1 = 0, digit2;
 
-	while (digit1 <= 9)
+	while (digit1 <= 99)
 	{
-		digit2 = 0;
-		while (digit2 <= 8)
+		digit2 = digit1;
+		while (digit2 <= 99)
 		{
-			digit3 = 0;
-			while (digit3 <= 9)
+			if (digit2 != digit1)
 			{
-				digit4 = 0;
-				while (digit4 <= 9)
+				putchar((digit1 / 10) + 48);
+				putchar((digit1 % 10) + 48);
+				putchar(' ');
+				putchar((digit2 / 10) + 48);
+				putchar((digit2 % 10) + 48);
+		
+				if (digit1 != 98 || digit2 != 99)
 				{
-					if (digit1 == digit3 && digit2 == digit4)
-                    {
-                        digit4++;
-                        continue;
-					}
-					if (((digit1 * 10) + digit2) >= ((digit3 * 10) + digit4))
-                    {
-                        digit4++;
-                        continue;
-					}
-					putchar(digit1 + 48);
-					putchar(digit2 + 48);
-					putchar(' ');
-					putchar(digit3 + 48);
-					putchar(digit4 + 48);
-
-					if (((digit1 * 10) + digit2) == 98){
-                        if (((digit3 * 10) + digit4) == 99)
-                        {
-                         digit4++;
-                         continue;
-                        }
-					}
-					digit4++;
 					putchar(',');
 					putchar(' ');
 				}
-				digit3++;
 			}
-            digit2++;
-        }
-        digit1++;
-    }
-    putchar('\n');
+			digit2++;
+		}
+		digit1++;
+	}
+	putchar('\n');
 
 	return (0);
 }
