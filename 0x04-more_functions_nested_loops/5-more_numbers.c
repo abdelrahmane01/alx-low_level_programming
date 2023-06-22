63a0f7583706e45e1d5d98d985e553cadd461962
abdelrahmane01 <abdelrahmanemam01@gmail.com>
2023-06-22 06:13:12 -0700

    bye

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 - 14
+ *	ten times
+ *	using _putchar function
+ *
+ * Return: always 0 (success)
+*/
+
+void more_numbers(void)
+{
+	int i, k;
+
+	for (k = 0; k < 10; k++)
+	{
+		for (i = 0; i <= 14; i++)
+			_putchar(i + 48);
+	}
+	_putchar('\n');
+}
