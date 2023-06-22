bb20fabd1821292863ade6bfae218754c44ff785
abdelrahmane01 <abdelrahmanemam01@gmail.com>
2023-06-22 06:03:21 -0700

    bye

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0 - 9
+ *	using _putchar function except
+ *	2 and 4
+ *
+ * Return: always 0 (success)
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+		if (i == 2 || i == 4)
+			continue;
+		_putchar(i + 48);
+	_putchar('\n');
+}
