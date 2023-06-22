efe6fca5f6ff0e7bce0a4fc85bbd5c8b485255ce
abdelrahmane01 <abdelrahmanemam01@gmail.com>
2023-06-22 06:28:06 -0700

    bye

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,20 +2,28 @@
 
 /**
  * more_numbers - prints 0 - 14
- *	ten times
- *	using _putchar function
+ *      ten times
+ *      using _putchar function
  *
  * Return: always 0 (success)
 */
 
 void more_numbers(void)
 {
-	int i, k;
+	int i, k, z;
 
 	for (k = 0; k < 10; k++)
 	{
 		for (i = 0; i <= 14; i++)
-			_putchar(i + 48);
+		{
+			z = i;
+			if (i > 9)
+			{
+				_putchar(1 + 48);
+				z = i % 2;
+			}
+			_putchar(z + 48);
+		}
 		_putchar('\n');
 	}
 }
