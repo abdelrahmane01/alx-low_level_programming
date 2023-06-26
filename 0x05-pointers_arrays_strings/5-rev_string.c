661e08554386f23a39239dcf98d12c02cc1955d3
abdelrahmane01 <abdelrahmanemam01@gmail.com>
2023-06-26 04:31:51 -0700

    bye

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that prints a string, in reverse
+ *
+ * @s: pointer to a string to print
+ *
+ * Return: Nothing
+ */
+
+void rev_string(char *s)
+{
+	int x = 0;
+
+	while (*s != '\0')
+	{
+		x++;
+		s++;
+	}
+	while (x)
+	{
+		s--;
+		_putchar(*s + 0);
+		x--;
+	}
+	_putchar('\n');
+}
