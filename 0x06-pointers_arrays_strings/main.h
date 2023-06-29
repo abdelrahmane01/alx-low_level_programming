2c5f96ab18078f2e075f92018d2fa275f2d5ce74
abdelrahmane01 <abdelrahmanemam01@gmail.com>
2023-06-29 03:45:55 -0700

    bye

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -19,5 +19,6 @@ void print_times_table(int n);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
 
 #endif
